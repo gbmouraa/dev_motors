ff3c2610c6a02341c11e081f38f837dc351fb997
gbmouraa <gabrielmoura-cma@hotmail.com>
2025-09-15 21:22:28 -0300

    feat: enhance Home component layout with Container, InputSearchCar, and Categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { CarouselPlugin } from "../components/hero-carousel";
+import { Container } from "@/components/container";
+import { CarouselPlugin } from "./_components/hero-carousel";
+import { InputSearchCar } from "./_components/input-search-car";
+import { Categories } from "./_components/categories";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-sky-50 min-h-[calc(100vh-56px)]">
       <CarouselPlugin />
+      <Container>
+        <section className="flex w-full flex-col items-center">
+          <InputSearchCar />
+          <Categories />
+        </section>
+        <section></section>
+      </Container>
     </main>
   );
 }
