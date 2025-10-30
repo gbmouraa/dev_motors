d7f2b2266a6f2a587863855dee202b4acd7fcd47
gbmouraa <gabrielmoura-cma@hotmail.com>
2025-10-29 21:09:21 -0300

    refactor: improve loading state handling in favorites page; enhance error handling and loading feedback

--- src/app/(admin)/favorites/page.tsx
+++ src/app/(admin)/favorites/page.tsx
@@ -5,19 +5,25 @@ import { CarItemProps } from "@/types/car";
 import { AuthContext } from "@/context/auth-context";
 import { getFavoritesCars, removeCarFromFavorites } from "@/lib/firebase/car";
 import { CarItem } from "@/components/car-item";
+import { Loader } from "../../../components/loader";
+import { Car } from "lucide-react";
 
 export default function Favorites() {
   const [cars, setCars] = useState<CarItemProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchCars = async () => {
-      if (!user) return;
+      try {
+        if (!user) return;
 
-      const res = await getFavoritesCars(user.uid);
-      if (res) {
-        setCars(res);
+        const res = await getFavoritesCars(user.uid);
+        if (res) setCars(res);
+      } catch (err) {
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -30,23 +36,42 @@ export default function Favorites() {
     setCars(cars.filter((item) => item.id !== carId));
   };
 
+  if (isLoading) {
+    return (
+      <div className="relative h-[200px]">
+        <Loader bg="bg-[#ECEDF2]" />
+      </div>
+    );
+  }
+
   return (
-    <section className="flex flex-wrap flex-col gap-3 md:flex-row">
-      {cars.map((item) => (
-        <CarItem
-          id={item.id}
-          key={item.id}
-          name={item.name}
-          model={item.model}
-          year={item.year}
-          km={item.km}
-          city={item.city}
-          price={item.price}
-          images={item.images}
-          isOnFavoritePage
-          onClick={() => handleDeleteCarFromFavorites(item.id)}
-        />
-      ))}
-    </section>
+    <>
+      {cars.length === 0 ? (
+        <div>
+          <p className="flex items-center text-gray-600 gap-2">
+            <Car size={20} />
+            Você não possui nenhum anuncio favoritado.{" "}
+          </p>
+        </div>
+      ) : (
+        <section className="flex flex-wrap flex-col gap-3 md:flex-row">
+          {cars.map((item) => (
+            <CarItem
+              id={item.id}
+              key={item.id}
+              name={item.name}
+              model={item.model}
+              year={item.year}
+              km={item.km}
+              city={item.city}
+              price={item.price}
+              images={item.images}
+              isOnFavoritePage
+              onClick={() => handleDeleteCarFromFavorites(item.id)}
+            />
+          ))}
+        </section>
+      )}
+    </>
   );
 }
